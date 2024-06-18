7ad4726f54a6f16f0f758688764dd126172d6ebb
unknown <maru4737@naver.com>
2024-06-18 22:22:59 +0900

    인덱스컨트롤러(첫화면띄우기) 추가 및 로그인컨트롤러 추가

--- src/main/java/com/example/petToyShop/Login/controller/LoginController.java
+++ src/main/java/com/example/petToyShop/Login/controller/LoginController.java
@@ -2,10 +2,12 @@ package com.example.petToyShop.Login.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/Login")
 @Controller
 public class LoginController {
-    @GetMapping("/Login")
+    @GetMapping("/LoginPage")
     public String home() {
         return "test";
     }
