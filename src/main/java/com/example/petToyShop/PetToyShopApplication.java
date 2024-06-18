0744577d559e9b56c85617856d9598fb28b8c02d
unknown <maru4737@naver.com>
2024-06-18 21:22:31 +0900

    20240618 pet toy project

---
+++ src/main/java/com/example/petToyShop/PetToyShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.petToyShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetToyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetToyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/petToyShop/PetToyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petToyShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetToyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
