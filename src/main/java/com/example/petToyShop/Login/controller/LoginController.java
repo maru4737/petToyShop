82fc8956e26b7715bb2a5b3b80c609e09153f741
unknown <maru4737@naver.com>
2024-06-18 22:17:39 +0900

    1

--- src/main/java/com/example/petToyShop/Login/controller/LoginController.java
+++ src/main/java/com/example/petToyShop/Login/controller/LoginController.java
@@ -1,2 +1,12 @@
-package com.example.petToyShop.Login.controller;public class LoginController {
+package com.example.petToyShop.Login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/Login")
+    public String home() {
+        return "test";
+    }
 }
