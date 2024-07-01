3920c997ac2e893d5107b4a61eb774ba0374b310
maru4737 <maru4737@naver.com>
2024-07-01 18:29:12 +0900

    로그인 개발중

--- src/main/java/com/example/petToyShop/Login/controller/LoginController.java
+++ src/main/java/com/example/petToyShop/Login/controller/LoginController.java
@@ -3,6 +3,9 @@ package com.example.petToyShop.Login.controller;
 import com.example.petToyShop.Login.mapper.UserMapper;
 import com.example.petToyShop.Login.service.UserService;
 import com.example.petToyShop.Login.vo.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,10 +32,15 @@ public class LoginController {
     }
 
     @PostMapping("/LoginStart")
-    public String LoginStart(User user) {
+    public String LoginStart(HttpServletRequest request, HttpServletResponse response, User user, Model model) {
         System.out.println(user);
         User LoginUserVo = userService.LoginStart(user);
         System.out.println(LoginUserVo);
+
+        if(LoginUserVo.getId() != null){
+            HttpSession loginSess = request.getSession(false);
+            loginSess.setAttribute("user",LoginUserVo);
+        }
         //System.out.println("Requested userId: " + userService.getUserById(user));
 
         return "index";
